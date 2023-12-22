19ff5b073f99786e34d10a17fa3347541504d1ff
David Dickinson <davidrd123@gmail.com>
2023-12-21 17:11:28 -0800

    Finished 'Build a Blog' tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://davidrd123-astro-blog-tutorial.netlify.app"
+  site: "https://davidrd123-astro-blog-tutorial.netlify.app",
+  integrations: [preact()]
 });
